758c27c236ba192cf098e16be230046818d13d28
sashadotsenko <dots.alex96@gmail.com>
2021-08-07 11:08:06 +0300

    T-5: Setup UI gems and basic pages (bootstrap + homepage)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_in" => "devise/sessions#new"
+    get "/users/sign_up" => "devise/registrations#new"
+  end
+
+  root 'static_pages#home'
 end
