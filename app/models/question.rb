90f6b6d522d2b298dcd86a622fa959dc60e3032c
sashadotsenko <dots.alex96@gmail.com>
2021-08-23 13:58:09 +0300

    Added gem annotate

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: questions
+#
+#  id            :bigint           not null, primary key
+#  answer_type   :string           not null
+#  answers_count :integer          default(1)
+#  body          :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :bigint
+#
+# Indexes
+#
+#  index_questions_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :questions_tests, dependent: :nullify
--- app/models/questions_test.rb
+++ app/models/questions_test.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: questions_tests
+#
+#  id          :bigint           not null, primary key
+#  question_id :bigint           not null
+#  test_id     :bigint           not null
+#
 class QuestionsTest < ApplicationRecord
   belongs_to :question
   belongs_to :test
