f2a69b87bbd53afa6d32bcb9d893febdf03326da
juliabychok <juliabychok130@gmail.com>
2021-08-25 09:51:39 +0300

    add profile to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_20_162023) do
+ActiveRecord::Schema.define(version: 2021_08_24_184713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
